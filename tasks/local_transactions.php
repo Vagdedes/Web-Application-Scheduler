bfe96394f5b407855345403eb5a59408c03a890a
Evangelos <contact@vagdedes.com>
2024-07-19 02:14:57 +0300

    Update local_transactions.php

--- tasks/local_transactions.php
+++ tasks/local_transactions.php
@@ -3,7 +3,7 @@
 function local_transactions(): string
 {
     $bool = update_paypal_storage(0, 1, true);
-    $bool |= update_stripe_storage();
+    //$bool |= update_stripe_storage(); Not needed because Polymart now provides API for purchases
     $account = new Account();
     $account->getPaymentProcessor()->run();
     return strval($bool);
