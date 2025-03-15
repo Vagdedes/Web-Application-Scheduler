6d6a548abfae34ca65eafc1ee5e330d8e45b60a4
Evangelos <vaggdedes@gmail.com>
2025-03-15 17:43:24 +0200

    Combined separated methods

--- tasks/application_big_manage_history_scheduler.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-function application_big_manage_history_scheduler(): bool
-{
-    return BigManageHistoryAsyncScheduler::run();
-}
--- tasks/application_big_manage_prompts_scheduler.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-function application_big_manage_prompts_scheduler(): bool
-{
-    return BigManagePromptsAsyncScheduler::run();
-}
