9380f306ec298422b8744b0b402280dd11f62d8c
Evangelos <vaggdedes@gmail.com>
2025-04-06 00:53:51 +0300

    Update transactions.php

--- tasks/transactions.php
+++ tasks/transactions.php
@@ -3,7 +3,7 @@
 function transactions(int $pastDays): string
 {
     $bool = update_paypal_storage(0, $pastDays, true);
-    //$bool |= update_stripe_storage(); Not needed because Polymart now provides API for purchases
+    $bool |= update_stripe_storage();
     $account = new Account();
     $account->getPaymentProcessor()->run();
     return strval($bool);
